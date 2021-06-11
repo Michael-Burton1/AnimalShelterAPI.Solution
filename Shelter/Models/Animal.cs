650715d14a4c1218549e674d741a7ff59535a82f
Michael Burton <micbur1@gmail.com>
2021-06-11 14:17:17 -0700

    add animal class and properties to project

---
+++ Shelter/Models/Animal.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Shelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId {get; set;}
+    [Required]
+    public string Name {get; set;}
+    [Required]
+    public int Age {get; set;}
+    [Required]
+    public string Type {get; set;}
+    [Required]
+    public string Sex {get; set;}
+    [Required]
+    public bool Available {get; set;}
+  }
+}
