4d725328ff3ae36a030203b480865006b106bfb7
Michael Burton <micbur1@gmail.com>
2021-06-13 16:54:50 -0700

    add 'avalible' property into seed data

--- Shelter/Models/ShelterContext.cs
+++ Shelter/Models/ShelterContext.cs
@@ -8,18 +8,18 @@ namespace Shelter.Models
     {
       builder.Entity<Animal>()
           .HasData(
-              new Animal { AnimalId = 1, Name = "John", Age = 15, Type="cat", Sex = "Male"},
-              new Animal { AnimalId = 2, Name = "Robb", Age = 15, Type="cat", Sex = "Male" },
-              new Animal { AnimalId = 3, Name = "Sansa", Age = 11, Type="cat", Sex = "Female"},
-              new Animal { AnimalId = 4, Name = "Bran", Age = 7, Type="cat", Sex = "Male"},
-              new Animal { AnimalId = 5, Name = "Arya", Age = 9, Type="cat", Sex = "Female"},
-              new Animal { AnimalId = 6, Name = "Rickon", Age = 3, Type="cat", Sex = "Male" },
-              new Animal { AnimalId = 7, Name = "Ghost", Age = 1, Type="dog", Sex = "Male"},
-              new Animal { AnimalId = 8, Name = "Grey Wind", Age = 1, Type="dog", Sex = "Male" },
-              new Animal { AnimalId = 9, Name = "Lady", Age = 1, Type="dog", Sex = "Female" },
-              new Animal { AnimalId = 10, Name = "Nymeria", Age = 1, Type="dog", Sex = "Female"},
-              new Animal { AnimalId = 11, Name = "Summer", Age = 1, Type="dog", Sex = "Male" },
-              new Animal { AnimalId = 12, Name = "Shaggy Dog", Age = 1, Type="dog", Sex = "Male" }
+              new Animal { AnimalId = 1, Name = "John", Age = 15, Type="cat", Sex = "Male", Availible= true},
+              new Animal { AnimalId = 2, Name = "Robb", Age = 15, Type="cat", Sex = "Male", Availible= false},
+              new Animal { AnimalId = 3, Name = "Sansa", Age = 11, Type="cat", Sex = "Female", Availible= true},
+              new Animal { AnimalId = 4, Name = "Bran", Age = 7, Type="cat", Sex = "Male", Availible= true},
+              new Animal { AnimalId = 5, Name = "Arya", Age = 9, Type="cat", Sex = "Female", Availible= true},
+              new Animal { AnimalId = 6, Name = "Rickon", Age = 3, Type="cat", Sex = "Male", Availible= false},
+              new Animal { AnimalId = 7, Name = "Ghost", Age = 1, Type="dog", Sex = "Male", Availible= true},
+              new Animal { AnimalId = 8, Name = "Grey Wind", Age = 1, Type="dog", Sex = "Male", Availible= false},
+              new Animal { AnimalId = 9, Name = "Lady", Age = 1, Type="dog", Sex = "Female", Availible= false},
+              new Animal { AnimalId = 10, Name = "Nymeria", Age = 1, Type="dog", Sex = "Female", Availible= true},
+              new Animal { AnimalId = 11, Name = "Summer", Age = 1, Type="dog", Sex = "Male", Availible= false},
+              new Animal { AnimalId = 12, Name = "Shaggy Dog", Age = 1, Type="dog", Sex = "Male", Availible= false}
               
           );
     }
