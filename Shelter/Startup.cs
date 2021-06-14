8294a2b45dbac3fdbb785b09d652164fd35a5d50
Michael Burton <micbur1@gmail.com>
2021-06-13 22:47:07 -0700

    added code to start up to be able to incorporate Swagger in project

--- Shelter/Startup.cs
+++ Shelter/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Shelter.Models;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
 
 namespace Shelter
 {
@@ -24,11 +28,27 @@ namespace Shelter
             services.AddDbContext<ShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MessageBoard API",
+                    Description = "Message Board ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://homestarrunner.com")
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
